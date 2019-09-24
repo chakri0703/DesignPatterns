e6660631502e13b630423ba0dc6c888dc7c6c68c
chakravarthig <gchakravarthia8@gmail.com>
2019-09-24 10:45:16 +0530

    [chakri] Add. flyWeightFactory to reduce the memoryUsage

---
+++ flyWeight/flyWeight.js
@@ -0,0 +1,83 @@
+var Task = function (data) {
+    this.flyweight=FlyWeightFactory.get(data.project,data.priority,data.user,data.completed);
+    this.name = data.name;
+    // this.priority = data.priority;
+    // this.project = data.project;
+    // this.user = data.user
+    // this.completed = data.completed
+}
+function FlyWeight(project,priority,user,completed){
+    this.priority=priority;
+    this.project=project;
+    this.user=user;
+    this.completed=completed 
+}
+
+var FlyWeightFactory=function(){
+    var flyweights={};
+    var get=function(project,priority,user,completed){
+        if(!flyweights[project,priority,user,completed]){
+            flyweights[project,priority,user,completed]=new FlyWeight(project,priority,user,completed)
+            return flyweights[project,priority,user,completed];
+        }
+    };
+    var getCount=function(){
+        var count=0;
+        for(var f in flyweights)count++;
+
+        return count
+    };
+
+    return{
+         get:get,
+         getCount:getCount 
+    }
+}()
+
+function TaskCollection() {
+    var tasks = {};
+    var count = 0;
+    var add = function (data) {
+        tasks[data.name] = new Task(data);
+        count++;
+    }
+    var get = function (name) {
+        return tasks[name];
+    };
+    var getCount = function () {
+        return count;
+    };
+    return {
+        add: add,
+        get: get,
+        getCount: getCount
+
+    };
+}
+var tasks = new TaskCollection();
+
+var projects = ['none', 'courses', 'training', 'project'];
+var priorities = [1, 2, 3, 4, 5];
+var users = ['jon', 'chakri', 'shyam'];
+var completed = [true, false];
+
+var initialMemory = process.memoryUsage().heapUsed;
+
+for (let i = 0; i < 100000; i++) {
+    tasks.add({
+        name: 'task' + i,
+        priority: priorities[Math.floor((Math.random() * 5))],
+        project: projects[Math.floor((Math.random() * 4))],
+        user: users[Math.floor((Math.random() * 3))],
+        completed: completed[Math.floor((Math.random() * 2 ))]
+    })
+}
+
+var afterMemory=process.memoryUsage().heapUsed;
+console.log("used Memory "+(afterMemory-initialMemory)/1000000+" MB");
+// console.log(process.memoryUsage());
+console.log("tasks:"+tasks.getCount());
+
+console.log("flyweights :"+FlyWeightFactory.getCount());
+
+
